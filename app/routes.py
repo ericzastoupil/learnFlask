c96dd5f8f87731ac824360ee87e100c95506f823
ericzastoupil <ericzastoupil@users.noreply.github.com>
2021-08-14 14:38:08 -1000

    added newer/older pagination

--- app/routes.py
+++ app/routes.py
@@ -152,4 +152,8 @@ def unfollow(username):
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
-    return render_template('index.html', title='Explore', posts=posts.items)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.pre_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
